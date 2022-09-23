c3b8c6251fd2aa3c111b0a5cc4451e1e4c5dcdb5
Rutvik Patel <rutvik.patel@bacancy.com>
2022-09-23 11:20:39 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './app/shared/services/registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './app/app-router'
+import store from './app/app-state'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
