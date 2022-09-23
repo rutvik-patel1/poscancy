8714c89c1cc8f18a53304c5c175786cb85f0f1f4
Rutvik Patel <rutvik.patel@bacancy.com>
2022-09-23 10:49:41 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import './app/shared/services/registerServiceWorker'
 import router from './router'
 import store from './store'
 
