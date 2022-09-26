c95ab44c10e2505711dc2d97d0df62b1fdb98daa
Rutvik Patel <rutvik.patel@bacancy.com>
2022-09-26 14:20:13 +0530

    add: detailed post view and comment section added

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,7 +3,10 @@
     <Navbar ></Navbar>
 
     <v-main color="primary" :style="{background: $vuetify.theme.themes['light'].background}" >
+      
+      <detailed-post></detailed-post>
       <HelloWorldVue/>
+
     </v-main>
    
   </v-app>
@@ -12,13 +15,14 @@
 <script>
 import Navbar from './shared/components/Navbar.vue';
 import HelloWorldVue from './shared/components/HelloWorld.vue';
-
+import DetailedPost from './posts/DetailedPost.vue';
 export default {
   name: "App",
 
   components: {
     Navbar,
-    HelloWorldVue
+    HelloWorldVue,
+    DetailedPost
   },
 
   data: () => ({
