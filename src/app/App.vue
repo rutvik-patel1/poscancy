7c55bac43708c6716fe5b1aa4707bd8dfaf3329d
Rutvik Patel <rutvik.patel@bacancy.com>
2022-10-07 10:44:22 +0530

    v-alert component added

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,35 @@
 <template>
-      <v-app>
-        <router-view></router-view>
-      </v-app>
+  <v-app>
+    <v-alert
+      border="left"
+      dismissible
+      style="z-index: 999; position: fixed; top: 70px; right: 10px"
+      width="400px"
+      :type="type"
+      v-if="isAlert"
+      >{{ message }}</v-alert
+    >
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-
+  name: "App",
+  data() {
+    return {};
+  },
+  computed: {
+    isAlert() {
+      return this.$store.state.showAlert;
+    },
+    type() {
+      return this.$store.state.type;
+    },
+    message() {
+      return this.$store.state.alertMsg;
+    },
+  },
 };
 </script>
 
