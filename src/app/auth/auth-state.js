ff2910af12a929bfe63f7834f044d7e0b024081a
chetan <16it099@charusat.edu.in>
2022-10-12 18:15:58 +0530

    add: username,photo display & addfriends tab

--- src/app/auth/auth-state.js
+++ src/app/auth/auth-state.js
@@ -1,26 +1,25 @@
- 
-    const state = {
-             userId:''
-        };
-    const  mutations = {
-        setUserId(state,id){
-            state.userId = id;
-            console.log("User ID =>",state.userId);
-        }
-    };
-    const actions = {
-        setUserId({commit},payload){
-            commit('setUserId',payload.userId);
-        }
-    };
-    const getters = {
-        getUserId(state){
-            return state.userId;
-        }
-    };
+const state = {
+    userId: ''
+};
+const mutations = {
+    setUserId(state, id) {
+        state.userId = id;
+        console.log("User ID =>", state.userId);
+    }
+};
+const actions = {
+    setUserId({ commit }, payload) {
+        commit('setUserId', payload.userId);
+    }
+};
+const getters = {
+    getUserId(state) {
+        return state.userId;
+    }
+};
 
 export default {
-    namespaced:true,
+    namespaced: true,
     state,
     mutations,
     actions,
